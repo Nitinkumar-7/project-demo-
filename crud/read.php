933ab47ca8c41d776cc66e946d6c15cfab4e73de
Nitinkumar-7 <nitinkandwal0@gmail.com>
2023-01-20 14:05:24 +0530

    curd operations

---
+++ crud/read.php
@@ -0,0 +1,73 @@
+<?php
+    require 'database.php';
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: index.php");
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM customers where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+    }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Read a Customer</h3>
+                    </div>
+                     
+                    <div class="form-horizontal" >
+                      <div class="control-group">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['name'];?>
+                            </label>
+                        </div>
+                      </div>
+                      <div class="control-group">
+                        <label class="control-label">Email Address</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['email'];?>
+                            </label>
+                        </div>
+                      </div>
+                      <div class="control-group">
+                        <label class="control-label">Mobile Number</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['mobile'];?>
+                            </label>
+                        </div>
+                      </div>
+                        <div class="form-actions">
+                          <a class="btn" href="index.php">Back</a>
+                       </div>
+                     
+                      
+                    </div>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
